222e814533c368b80cdfdd27be8c6908b5b77686
Teguh Wijiyanto <teguh.wijiyanto2@gmail.com>
2025-04-23 16:55:17 +0700

    Update elements

--- packages/ui/src/ui-component/dialog/AboutDialog.jsx
+++ packages/ui/src/ui-component/dialog/AboutDialog.jsx
@@ -1,8 +1,7 @@
 import { createPortal } from 'react-dom'
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Dialog, DialogContent, DialogTitle, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material'
-import moment from 'moment'
+import { Dialog, DialogContent, DialogTitle, TableContainer, Table, TableRow, TableCell, TableBody, Paper } from '@mui/material'
 import axios from 'axios'
 import { baseURL } from '@/store/constant'
 
@@ -56,30 +55,21 @@ const AboutDialog = ({ show, onCancel }) => {
             aria-describedby='alert-dialog-description'
         >
             <DialogTitle sx={{ fontSize: '1rem' }} id='alert-dialog-title'>
-                Flowise Version
+                SimplifyAI: Making Smart Automation Easier with Generative AI
             </DialogTitle>
             <DialogContent>
                 {data && (
                     <TableContainer component={Paper}>
                         <Table aria-label='simple table'>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Current Version</TableCell>
-                                    <TableCell>Latest Version</TableCell>
-                                    <TableCell>Published At</TableCell>
-                                </TableRow>
-                            </TableHead>
                             <TableBody>
                                 <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                     <TableCell component='th' scope='row'>
-                                        {data.currentVersion}
+                                        SimplifyAI is all about making smart automation simple. We&rsquo;ve built a platform that lets you
+                                        quickly create chatbots and AI helpers using the cool tech of generative AI – like those big
+                                        language models you hear about. <p></p>Think of it as a no-code/low-code way to get AI working for
+                                        you, without you, without you, without needing to be a coding whiz. <p></p>Current version:
+                                        SimplifyAI v1.0
                                     </TableCell>
-                                    <TableCell component='th' scope='row'>
-                                        <a target='_blank' rel='noreferrer' href={data.html_url}>
-                                            {data.name}
-                                        </a>
-                                    </TableCell>
-                                    <TableCell>{moment(data.published_at).fromNow()}</TableCell>
                                 </TableRow>
                             </TableBody>
                         </Table>
